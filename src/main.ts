6d4c7f38c07801fcc4db9f510ec8c3b4d4335dc1
seokheejang <shjang@dkargo.io>
2023-04-25 19:01:59 +0900

    setting import absolute path

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import "module-alias/register";
-import { Wallet } from "@eth-infinitism-bundler/ethers";
+import { Wallet } from "@eth-infinitism-bundler/node_modules/ethers";
+import {
+  DeterministicDeployer,
+  HttpRpcClient,
+  SimpleAccountAPI,
+} from "@eth-infinitism-bundler/packages/sdk";
+
+import { runBundler } from "@eth-infinitism-bundler/packages/bundler/src/runBundler";
+
+class Runner {
+  bundlerProvider!: HttpRpcClient;
+  accountApi!: SimpleAccountAPI;
+}
 
 function main() {
-  console.log("Hello AA");
   const OwnerA = new Wallet("0x".padEnd(66, "1"));
   console.log("OwnerA", OwnerA);
+
+  console.log("OwnerA", new HttpRpcClient("", "", 15));
+
+  // const argv = [
+  //   "node",
+  //   "exec",
+  //   "--config",
+  //   "./localconfig/bundler.config.json",
+  //   "--unsafe",
+  // ];
+
+  // const run = runBundler(argv);
 }
 
 main();
